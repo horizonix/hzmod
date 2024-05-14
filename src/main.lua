0d5e09161753ac80956bf719b0ebaa18161ba762
horizonix <git@zack.pevcot.net>
2024-05-14 16:20:51 +0100

    Update main.lua

--- src/main.lua
+++ src/main.lua
@@ -2,4 +2,9 @@
 --// Made by Horizonix Studios
 --/ Plugin Version: 1000/
 
+local hzuiData = {
+  root = script.Parent.UI;
+  core = true;
+};
+hzuiModule:ssd(hzuiData);
 print('Hello World! test')
